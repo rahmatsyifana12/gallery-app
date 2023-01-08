9d2a94db79e373e0bed08773ef7b4ec174787d75
alfianzv <alfian101@gmail.com>
2023-01-08 23:38:56 +0700

    refactor: added size on Images struct

--- models/memory.go
+++ models/memory.go
@@ -30,7 +30,7 @@ type CreateTagsDTO struct {
 
 type Images struct {
 	ID 			uint64 `gorm:"primaryKey;autoIncrement"`
-	Image 		string `gorm:"not null"`
+	Image 		string `gorm:"size:1023;not null"`
 	MemoryID 	uint64 `gorm:"not null"`
 	CreatedAt 	time.Time `gorm:"not null"`
 	UpdatedAt 	time.Time `gorm:"not null"`
