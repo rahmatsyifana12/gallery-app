d0fba64bd74a04d802aa169bd07514b33fa86f27
alfianzv <alfian101@gmail.com>
2023-01-08 23:33:54 +0700

    refactor: added size on Memory and Tags struct

--- models/memory.go
+++ models/memory.go
@@ -4,7 +4,7 @@ import "time"
 
 type Memory struct {
 	ID 			uint64 `gorm:"primaryKey;autoIncrement"`
-	Description string `gorm:"not null"`
+	Description string `gorm:"size:1023;not null"`
 	UserID 		uint64 `gorm:"not null"`
 	Tags 		[]Tags `gorm:"many2many:memory_tags;"`
 	Images 		[]Images `gorm:"foreignKey:MemoryID"`
@@ -18,7 +18,7 @@ type CreateMemoryDTO struct {
 
 type Tags struct {
 	ID 			uint64 `gorm:"primaryKey;autoIncrement"`
-	Name 		string `gorm:"not null"`
+	Name 		string `gorm:"size:255;not null"`
 	Memory 		[]Memory `gorm:"many2many:memory_tags;"`
 	CreatedAt 	time.Time `gorm:"not null"`
 	UpdatedAt 	time.Time `gorm:"not null"`
