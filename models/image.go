6c18f6e7971824c7a27c5666a553ffa049c120ef
Aan Rahmat <70148910+rahmatsyifana12@users.noreply.github.com>
2023-01-09 22:41:25 +0700

    Move image and tag model to each file of its model

---
+++ models/image.go
@@ -0,0 +1,15 @@
+package models
+
+import "time"
+
+type Images struct {
+	ID 			uint64 `gorm:"primaryKey;autoIncrement"`
+	Image 		string `gorm:"size:1023;not null"`
+	MemoryID 	uint64 `gorm:"not null"`
+	CreatedAt 	time.Time `gorm:"not null"`
+	UpdatedAt 	time.Time `gorm:"not null"`
+}
+
+type CreateImageDTO struct {
+	Image 		string `validate:"required,min=1,alphanum"`
+}
--- models/memory.go
+++ models/memory.go
@@ -6,7 +6,7 @@ type Memory struct {
 	ID 			uint64 `gorm:"primaryKey;autoIncrement"`
 	Description string `gorm:"size:1023;not null"`
 	UserID 		uint64 `gorm:"not null"`
-	Tags 		[]Tags `gorm:"many2many:memory_tags;"`
+	Tags 		[]Tag `gorm:"many2many:memory_tags;"`
 	Images 		[]Images `gorm:"foreignKey:MemoryID"`
 	CreatedAt 	time.Time `gorm:"not null"`
 	UpdatedAt 	time.Time `gorm:"not null"`
@@ -15,28 +15,3 @@ type Memory struct {
 type CreateMemoryDTO struct {
 	Description string `validate:"required,min=4,alphanum"`
 }
-
-type Tags struct {
-	ID 			uint64 `gorm:"primaryKey;autoIncrement"`
-	Name 		string `gorm:"size:255;not null"`
-	Memory 		[]Memory `gorm:"many2many:memory_tags;"`
-	CreatedAt 	time.Time `gorm:"not null"`
-	UpdatedAt 	time.Time `gorm:"not null"`
-}
-
-type CreateTagsDTO struct {
-	Name 		string `validate:"required,min=1,alphanum"`
-}
-
-type Images struct {
-	ID 			uint64 `gorm:"primaryKey;autoIncrement"`
-	Image 		string `gorm:"size:1023;not null"`
-	MemoryID 	uint64 `gorm:"not null"`
-	CreatedAt 	time.Time `gorm:"not null"`
-	UpdatedAt 	time.Time `gorm:"not null"`
-}
-
-type CreateImagesDTO struct {
-	Image 		string `validate:"required,min=1,alphanum"`
-}
-
